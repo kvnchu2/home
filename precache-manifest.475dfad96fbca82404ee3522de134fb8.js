2ba87b6bd4e8f86249c781c5eb5ce841f010b34d
Kevin Chu <kvnchu2@gmail.com>
2021-03-26 18:02:36 -0700

    Updates

---
+++ precache-manifest.475dfad96fbca82404ee3522de134fb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8404a80ce0f02109a5760135edbb29ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94c51572fd38e15e61f7",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1390d13147fcc8767529",
+    "url": "/static/js/2.95a996af.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.95a996af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c51572fd38e15e61f7",
+    "url": "/static/js/main.3e1cf4e6.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
