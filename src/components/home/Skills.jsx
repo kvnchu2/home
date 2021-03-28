296676c132d24a49d43486bb170e2cf7139eda59
Kevin Chu <kvnchu2@gmail.com>
2021-03-28 14:27:40 -0700

    add skills pictures

---
+++ src/components/home/Skills.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import SkillsTab from "./SkillsTab";
+import Row from "react-bootstrap/Row";
+import { Jumbotron, Container } from "react-bootstrap";
+import { useScrollPosition } from "../../hooks/useScrollPosition";
+
+function Skills({ heading, hardSkills, softSkills }) {
+  const skillsTabRef = React.useRef();
+  const [isScrolled, setIsScrolled] = React.useState(false);
+  //const navbarDimensions = useResizeObserver(navbarMenuRef);
+
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      if (!isScrolled && currPos.y - 400 < 0) setIsScrolled(true);
+    },
+    [],
+    skillsTabRef
+  );
+  return (
+    <img src=""></img>
+  );
+}
+
+export default Skills;
